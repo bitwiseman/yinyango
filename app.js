3c0270dac3e0a5d3ba95807a86fe59d9dfd69aeb
Mathieu Quinette <hickop@gmail.com>
2012-08-27 13:27:26 +0200

    Add logger.

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var app = express();
 app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
+    app.use(express.logger('dev'));
     app.use(express.static(__dirname + '/public'));
 });
 
