06a45ea7ec7abf27ec60da85519cbdef2361cf82
Mathieu Quinette <hickop@gmail.com>
2013-01-28 19:02:15 +0100

    Use namespace for login/register script.

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -5,99 +5,103 @@
  * @license  http://creativecommons.org/licenses/by-nc-sa/3.0/
  * @link     https://github.com/hickop/yinyango
  */
+'use strict';
+/* yylog {{{*/
+var yylog = {};
+/* jsonRequest {{{
+ * Simple ajax request expecting json in response.
+ *
+ * @param {String} url Destination url.
+ * @param {String} method Method to send data.
+ * @param {Object} data FormData Object to be sent by a POST.
+ * @param {Function} callback Callback function.
+ */
+yylog.jsonRequest = function (url, method, data, callback) {
+    var xhr = new XMLHttpRequest();
 
-(function () {
-    /* jsonRequest {{{
-     * Simple ajax request expecting json in response.
-     *
-     * @param {String} url Destination url.
-     * @param {String} method Method to send data.
-     * @param {Object} data FormData Object to be sent by a POST.
-     * @param {Function} callback Callback function.
-     */
-    function jsonRequest(url, method, data, callback) {
-        var xhr = new XMLHttpRequest();
-
-        if (callback === undefined) {
-            callback = data;
-            data = null;
+    if (callback === undefined) {
+        callback = data;
+        data = null;
+    }
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 &&
+                (xhr.status === 200 || xhr.status === 0)) {
+            callback(JSON.parse(xhr.responseText));
         }
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 &&
-                    (xhr.status === 200 || xhr.status === 0)) {
-                callback(JSON.parse(xhr.responseText));
+    };
+
+    xhr.open(method, url, true);
+    xhr.send(data);
+};
+/*}}}*/
+/* makeBinds {{{*/
+yylog.makeBinds = function () {
+    var loginform =     document.getElementById('loginform'),
+        registerform =  document.getElementById('registerform'),
+        errorlogin =    document.getElementById('errorlogin'),
+        errorexist =    document.getElementById('errorexist'),
+        errorname =     document.getElementById('errorname'),
+        regsuccess =    document.getElementById('regsuccess'),
+        login =         document.getElementById('login'),
+        register =      document.getElementById('register');
+
+    loginform.addEventListener('submit', function () {
+        var formdata = new FormData(this);
+        errorlogin.style.display = 'none';
+        yylog.jsonRequest('/login', 'POST', formdata, function (data) {
+            if (data) {
+                // Session is set, refresh the page.
+                window.location.href = '/';
+            } else {
+                // Error message.
+                errorlogin.style.display = 'block';
             }
-        };
+        });
+    }, false);
+    registerform.addEventListener('submit', function () {
+        var formdata = new FormData(this);
+        errorexist.style.display = 'none';
+        errorname.style.display = 'none';
+        regsuccess.style.display = 'none';
+        yylog.jsonRequest('/register', 'POST', formdata, function (data) {
+            if (data.success) {
+                // Registration successfull.
+                regsuccess.style.display = 'block';
+            } else if (data.error === 'name') {
+                errorname.style.display = 'block';
+            } else if (data.error === 'exist') {
+                errorexist.style.display = 'block';
+            }
+        });
+    }, false);
 
-        xhr.open(method, url, true);
-        xhr.send(data);
-    }
-    /*}}}*/
-    function makeBinds() {
-        var loginform =     document.getElementById('loginform'),
-            registerform =  document.getElementById('registerform'),
-            errorlogin =    document.getElementById('errorlogin'),
-            errorexist =    document.getElementById('errorexist'),
-            errorname =     document.getElementById('errorname'),
-            regsuccess =    document.getElementById('regsuccess'),
-            login =         document.getElementById('login'),
-            register =      document.getElementById('register');
-
-        loginform.addEventListener('submit', function () {
-            errorlogin.style.display = 'none';
-            jsonRequest('/login', 'POST', new FormData(this), function (data) {
-                if (data) {
-                    // Session is set, refresh the page.
-                    window.location.href = '/';
-                } else {
-                    // Error message.
-                    errorlogin.style.display = 'block';
-                }
-            });
-        }, false);
-        registerform.addEventListener('submit', function () {
-            errorexist.style.display = 'none';
-            errorname.style.display = 'none';
-            regsuccess.style.display = 'none';
-            jsonRequest('/register', 'POST', new FormData(this), function (data) {
-                if (data.success) {
-                    // Registration successfull.
-                    regsuccess.style.display = 'block';
-                } else if (data.error === 'name') {
-                    errorname.style.display = 'block';
-                } else if (data.error === 'exist') {
-                    errorexist.style.display = 'block';
-                }
-            });
-        }, false);
-
-        login.addEventListener('click', function () {
-            errorlogin.style.display = 'none';
-            document.getElementById('register-scr').style.display = 'none';
-            document.getElementById('login-scr').style.display = 'block';
-            loginform.username.focus();
-        }, false);
-        register.addEventListener('click', function () {
-            errorexist.style.display = 'none';
-            errorname.style.display = 'none';
-            regsuccess.style.display = 'none';
-            document.getElementById('login-scr').style.display = 'none';
-            document.getElementById('register-scr').style.display = 'block';
-            registerform.username.focus();
-        }, false);
-    }
-
-    function init() {
-        var loginform = document.getElementById('loginform');
-
-        makeBinds();
-        // Show login screen.
+    login.addEventListener('click', function () {
+        errorlogin.style.display = 'none';
+        document.getElementById('register-scr').style.display = 'none';
         document.getElementById('login-scr').style.display = 'block';
-        // Focus username.
         loginform.username.focus();
-    }
+    }, false);
+    register.addEventListener('click', function () {
+        errorexist.style.display = 'none';
+        errorname.style.display = 'none';
+        regsuccess.style.display = 'none';
+        document.getElementById('login-scr').style.display = 'none';
+        document.getElementById('register-scr').style.display = 'block';
+        registerform.username.focus();
+    }, false);
+};
+/*}}}*/
+/* init {{{*/
+yylog.init = function () {
+    var loginform = document.getElementById('loginform');
 
-    // Call init when DOM is loaded.
-    document.addEventListener('DOMContentLoaded', init(), false);
-
-}());
+    yylog.makeBinds();
+    // Show login screen.
+    document.getElementById('login-scr').style.display = 'block';
+    // Focus username.
+    loginform.username.focus();
+};
+/*}}}*/
+/*}}}*/
+// Call init when DOM is loaded.
+document.addEventListener('DOMContentLoaded', yylog.init(), false);
